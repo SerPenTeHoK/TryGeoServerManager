ca234622a68b7efc3a095986f0a24113c1e9c4da
SerPenTeHoK <sergey-for-fotos@mail.ru>
2016-02-19 00:37:48 +0300

    .

--- src/tests.java
+++ src/tests.java
@@ -1,7 +1,15 @@
 import it.geosolutions.geoserver.rest.GeoServerRESTPublisher;
 import it.geosolutions.geoserver.rest.GeoServerRESTReader;
+import it.geosolutions.geoserver.rest.decoder.RESTCoverage;
+import it.geosolutions.geoserver.rest.decoder.RESTDataStore;
+import it.geosolutions.geoserver.rest.decoder.RESTLayer;
+import it.geosolutions.geoserver.rest.encoder.datastore.GSDirectoryOfShapefilesDatastoreEncoder;
+import it.geosolutions.geoserver.rest.manager.GeoServerRESTStoreManager;
 
+import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.List;
 
 /**
@@ -13,6 +21,7 @@ public class tests {
         String RESTURL  = "http://localhost:8180/geoserver";
         String RESTUSER = "admin";
         String RESTPW   = "geoserver";
+        String workSpaceName = "myWorkspace1";
 
         GeoServerRESTReader reader = new GeoServerRESTReader(RESTURL, RESTUSER, RESTPW);
 
@@ -21,6 +30,14 @@ public class tests {
             List<String> nsn = reader.getNamespaceNames();
             for(String tmpStr : nsn)
                 System.out.println(tmpStr);
+            RESTLayer oraReg = reader.getLayer("OT", "REGIONS2010");
+            String strToLoad = oraReg.getResourceUrl();
+            RESTLayer layerTmp = oraReg.get
+            reader.getResource()
+
+            //RESTCoverage oraCover = reader.getCoverage("OT" );
+            //System.out.println(oraCover.toString());
+
         }
 
         GeoServerRESTPublisher publisher = new GeoServerRESTPublisher(RESTURL, RESTUSER, RESTPW);
@@ -43,6 +60,24 @@ public class tests {
         else
             System.out.println("not exists");
 
+        /*
+        boolean exists = reader.existsDatastore(workSpaceName, storeName);
+        if ( !exists ) {
+            File shapeFile = new File( "path/to/zipFile.zip" );
+            boolean published = publisher.publishShp(workspaceName,
+                    storeName, "TheLayerName", shapeFile, "EPSG:4326", "");
+        }
+        GeoServerRESTStoreManager storeManager;
+        reader = new GeoServerRESTReader(RESTURL, RESTUSER, RESTPW);
+        storeManager = new GeoServerRESTStoreManager( new URL(RESTURL), RESTUSER, RESTPW);
+        RESTDataStore rds = reader.getDatastore(workSpaceName, storeName);
+        GSDirectoryOfShapefilesDatastoreEncoder update = new GSDirectoryOfShapefilesDatastoreEncoder(rds);
+        update.setDescription("New Description goes here");
+        update.setEnabled(true);
+        update.setCharset(Charset.forName("UTF-8"));
+        update.setCreateSpatialIndex(true);
+        storeManager.update(workSpaceName, update);
+*/
 
     }
 }
