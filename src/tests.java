ca5643fc0d34fdc9ab16617930e128916b7cab80
SerPenTeHoK <sergey-for-fotos@mail.ru>
2016-02-18 23:31:10 +0300

    update

--- src/tests.java
+++ src/tests.java
@@ -2,6 +2,7 @@ import it.geosolutions.geoserver.rest.GeoServerRESTPublisher;
 import it.geosolutions.geoserver.rest.GeoServerRESTReader;
 
 import java.net.MalformedURLException;
+import java.util.List;
 
 /**
  * Created by SerP on 18.02.2016.
@@ -9,20 +10,30 @@ import java.net.MalformedURLException;
 public class tests {
 
     public static void main(String[] args) throws MalformedURLException {
-        String RESTURL  = "http://localhost:8083/geoserver";
+        String RESTURL  = "http://localhost:8180/geoserver";
         String RESTUSER = "admin";
         String RESTPW   = "geoserver";
 
         GeoServerRESTReader reader = new GeoServerRESTReader(RESTURL, RESTUSER, RESTPW);
+
+        if(reader.existGeoserver()) {
+            System.out.println("Есть ");
+            List<String> nsn = reader.getNamespaceNames();
+            for(String tmpStr : nsn)
+                System.out.println(tmpStr);
+        }
+
         GeoServerRESTPublisher publisher = new GeoServerRESTPublisher(RESTURL, RESTUSER, RESTPW);
 
         boolean created = publisher.createWorkspace("myWorkspace1");
         if(created)
-            System.out.printf("created");
+            System.out.println("created\\n");
 
         boolean ok = publisher.publishDBLayer("myWorkspace1", "pg_kids", "easia_gaul_0_aggr", "EPSG:4326", "default_polygon");
         if(ok)
-            System.out.printf("ok");
+            System.out.println("ok");
+        else
+            System.out.printf("not ok");
 
         boolean exists = reader.existsDatastore("myWorkspace", "myStore");
         if(exists)
